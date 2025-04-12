f0e87caa86b6d38662fbe20de8bba3726c1deae7
ayandhikaa <cmillatari@gmail.com@gmail.com>
2025-04-12 18:19:04 +0700

    selesai soal 1

---
+++ quiz/part2/soal1.js
@@ -0,0 +1,20 @@
+/*
+Diberikan sebuah function palindrome(kata) yang menerima satu parameter. Function tersebut akan mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan. Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'.
+*/
+
+function palindrome(kata) {
+    kataPisah = kata.split('');
+    kataBalik = kataPisah.reverse().join('');
+    if (kata === kataBalik) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+  
+  // TEST CASES
+  console.log(palindrome('katak')); // true
+  console.log(palindrome('blanket')); // false
+  console.log(palindrome('civic')); // true
+  console.log(palindrome('kasur rusak')); // true
+  console.log(palindrome('mister')); // false
